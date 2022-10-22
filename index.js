2ef55ec98ca5486ac9a6f83008a11bacd4193711
Martins CC <97920206+chizobaebuka@users.noreply.github.com>
2022-10-22 16:35:52 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const BodyEl = document.querySelector("body");
+
+BodyEl.addEventListener("mousemove", (event)=>{
+    const xPos = event.offsetX
+    const yPos = event.offsetY
+    const spanEl = document.createElement("span");
+    spanEl.style.left = xPos + "px"
+    spanEl.style.top = yPos + "px"
+    const size = Math.random() * 100;
+    spanEl.style.width = size + "px"
+    spanEl.style.height = size + "px"
+    BodyEl.appendChild(spanEl);
+    setTimeout(()=>{
+        spanEl.remove();
+    }, 3000)
+});
